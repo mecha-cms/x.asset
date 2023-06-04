30ac91c5bef572d6b7206dfd9f9e12818844cab8
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-04 11:47:47 +0700

    Rename Hook

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
 
+namespace x\asset {
+    function body($body) {
+        $script = \Hook::fire('script', [\Asset::join('text/javascript') . \Asset::join('text/js')], null, \Asset::class);
+        $js = \Hook::fire('asset.js', [\Asset::join('*.js')], null, \Asset::class);
+        return $body . $js . $script; // Put inline JS after remote JS
+    }
+    function content($content) {
+        return \strtr($content ?? "", [
+            '</body>' => \Hook::fire('body', [""], null, \Asset::class) . '</body>',
+            '</head>' => \Hook::fire('head', [""], null, \Asset::class) . '</head>'
+        ]);
+    }
+    function head($head) {
+        $style = \Hook::fire('style', [\Asset::join('text/css')], null, \Asset::class);
+        $css = \Hook::fire('asset.css', [\Asset::join('*.css')], null, \Asset::class);
+        return $head . $css . $style; // Put inline CSS after remote CSS
+    }
+    \Hook::set('body', __NAMESPACE__ . "\\body");
+    \Hook::set('content', __NAMESPACE__ . "\\content", 0);
+    \Hook::set('head', __NAMESPACE__ . "\\head");
+}
+
 namespace {
     function asset(...$lot) {
         return \count($lot) < 2 ? \Asset::get(...$lot) : \Asset::set(...$lot);
     }
-}
-
-namespace x {
-    function asset($content) {
-        return \strtr($content ?? "", [
-            '</body>' => \Hook::fire('asset:body', [""], null, \Asset::class) . '</body>',
-            '</head>' => \Hook::fire('asset:head', [""], null, \Asset::class) . '</head>'
-        ]);
-    }
-    \Hook::set('asset:head', function ($content) {
-        $style = \Hook::fire('asset:style', [\Asset::join('text/css')], null, \Asset::class);
-        $css = \Hook::fire('asset.css', [\Asset::join('*.css')], null, \Asset::class);
-        return $content . $css . $style; // Put inline CSS after remote CSS
-    });
-    \Hook::set('asset:body', function ($content) {
-        $script = \Hook::fire('asset:script', [\Asset::join('text/javascript') . \Asset::join('text/js')], null, \Asset::class);
-        $js = \Hook::fire('asset.js', [\Asset::join('*.js')], null, \Asset::class);
-        return $content . $js . $script; // Put inline JS after remote JS
-    });
-    \Hook::set('content', __NAMESPACE__ . "\\asset", 0);
 }
